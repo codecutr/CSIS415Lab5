db4599e4bfc3c8c5fe99a4e606ef7a0ad2fe88e0
Nick <nj20tudi@siena.edu>
2017-02-28 22:59:13 +0000

    commit from Cloud9

--- hello-world.php
+++ hello-world.php
@@ -4,8 +4,9 @@
 <?php
     $first = $_GET["first"];
     $last = $_GET["last"];
-
+    $shoe = $_GET["shoe"];
     echo "<p>Hello $first $last!</p>"; 
+    echo "<p>Nice Feet! your shoe size is $shoe!</p>"
 ?>
 </body>
 </html>
